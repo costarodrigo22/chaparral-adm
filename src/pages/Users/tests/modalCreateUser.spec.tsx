d459c466deabee34134e4cf4d262306ea44605a1
Rodrigo <rodrigocosta@motoca.com.br>
2024-09-24 17:44:50 -0300

    continuação dos testes de criação de usuários

--- src/pages/Users/tests/modalCreateUser.spec.tsx
+++ src/pages/Users/tests/modalCreateUser.spec.tsx
@@ -1,18 +1,29 @@
 import '@testing-library/jest-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
 import ModalAddUsers from '../components/ModalAddUsers';
-import { render } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { fireEvent, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import * as UsersServiceModule from '@/app/services/UsersService';
+
+// const mockHandleSubmit = vi.fn();
+
+vi.spyOn(UsersServiceModule.UsersService, 'addUsers').mockImplementation(
+  ({ name, email, password, profile_id }) => {
+    console.log('Mock addUsers chamado com:', {
+      name,
+      email,
+      password,
+      profile_id,
+    });
+
+    return Promise.resolve();
+    // return mockHandleSubmit({ name, email, password, profile_id });
+  },
+);
 
 const createQueryClient = () => new QueryClient();
 const queryClient = createQueryClient();
-
-// const createUserMock = vi.fn().mockImplementation(() => {
-//   useMutation({ mutationFn: UsersService.addUsers });
-// });
-
-// vi.spyOn();
 
 describe('Modal Create User', () => {
   test('Should be able render modal in the screen', () => {
@@ -28,26 +39,41 @@ describe('Modal Create User', () => {
   });
 
   test('Should be able create a user', async () => {
-    // const { getByTestId, getByPlaceholderText, getByText } = render(
-    //   <QueryClientProvider client={queryClient}>
-    //     <ModalAddUsers open={true} onClose={() => {}} />
-    //   </QueryClientProvider>,
-    // );
+    const { getByTestId, getByPlaceholderText, getByText } = render(
+      <QueryClientProvider client={queryClient}>
+        <ModalAddUsers open={true} onClose={() => {}} />
+      </QueryClientProvider>,
+    );
+
+    const nameInput = getByPlaceholderText('Nome');
+    await userEvent.type(nameInput, 'Rodrigo Costa');
+
+    const emailInput = getByPlaceholderText('Digite seu e-mail');
+    await userEvent.type(emailInput, 'rodrigo@mail.com');
+
+    const selectTrigger = getByText('Selecione');
+    fireEvent.click(selectTrigger);
+    const admOptionSelected = getByText('Administrador');
+    fireEvent.click(admOptionSelected);
+
+    const passwordInput = getByPlaceholderText('******');
+    await userEvent.type(passwordInput, '123123');
+
+    fireEvent.submit(getByTestId('form-to-test'));
+
     // const form = getByTestId('form-to-test');
-    // const inputName = getByPlaceholderText('Nome');
-    // const inputEmail = getByPlaceholderText('Digite seu e-mail');
-    // const inputTypeProfile = 'Administrador';
-    // const inputPassword = getByPlaceholderText('******');
-    // await userEvent.type(inputName, 'Rodrigo Costa');
-    // await userEvent.type(inputEmail, 'costarodrigo22@gmail.com');
-    // await userEvent.type(inputPassword, '123123');
     // fireEvent.submit(form);
-    // expect(createUserMock).toHaveBeenCalled();
-    // expect(createUserMock).toHaveBeenCalledWith({
+
+    // console.log('ID: ', import.meta.env.VITE_ID_ADM);
+
+    console.log('Verificando se a função addUsers foi chamada');
+    // expect(mockHandleSubmit).toHaveBeenCalled();
+
+    // expect(mockHandleSubmit).toHaveBeenCalledWith({
     //   name: 'Rodrigo Costa',
-    //   email: 'costarodrigo22@gmail.com',
-    //   plassword: '123123',
-    //   profile_id: inputTypeProfile,
+    //   email: 'rodrigo@mail.com',
+    //   password: '123123',
+    //   profile_id: import.meta.env.VITE_ID_ADM, // Verifique se o ID do admin está correto
     // });
   });
 });
