cffb9e14474578ccca212d5bbe3707586d1bb461
HiagoBDev <hiagoborgaco@hotmail.com>
2024-10-30 12:16:40 -0300

    Alterações nos arquivos de configuração para novas dependencias e alguns ajustes gerais da aplicação

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
   darkMode: ['class'],
   content: ['./src/**/*.{html,js,ts,tsx}'],
   theme: {
     extend: {
+      boxShadow: {
+        sections: '1px 1px 3px 0px #00000a11',
+        sectionsDark: '1px 1px 3px 0px #fffff00a',
+      },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
@@ -51,7 +57,29 @@ export default {
           5: 'hsl(var(--chart-5))',
         },
       },
+      keyframes: {
+        'accordion-down': {
+          from: {
+            height: '0',
+          },
+          to: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+        },
+        'accordion-up': {
+          from: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+          to: {
+            height: '0',
+          },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate],
 };
